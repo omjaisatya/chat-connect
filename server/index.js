9a8727f95495f9c1c3e8a433bd7536fdac95673b
omjaisatya <satyaprakash451256@gmail.com>
2024-07-25 17:16:21 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -7,38 +7,12 @@ const Room = require("./models/Room");
 const { PORT } = require("./config/envConfig");
 const cors = require("cors");
 
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketio(server);
-// const Port = PORT || 5000;
-
-// app.use(cors({ origin: "*" }));
-
-// // Connect to the database
-// connectDB();
-
 const app = express();
 const server = http.createServer(app);
-
-// Updated CORS configuration
-const corsOptions = {
-  origin: "https://chat-connect-client.vercel.app",
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-// Configure socket.io with CORS
-const io = socketio(server, {
-  cors: {
-    origin: "https://chat-connect-client.vercel.app",
-    methods: ["GET", "POST"],
-  },
-});
-
+const io = socketio(server);
 const Port = PORT || 5000;
+
+app.use(cors({ origin: "*" }));
 
 // Connect to the database
 connectDB();
